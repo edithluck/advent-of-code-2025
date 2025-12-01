be9502c2b0b8a004ee8583ffea45ff006e0694ae
Edith Luckraft <edith@ianluckraft.co.uk>
2025-12-01 20:11:41 +0000

    day 1

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 def main():
-    print("Hello from advent-of-code-2025!")
+    zeros = 0
+    position = 50
+    
+    with open("codes.txt", "r") as file:
+        codes = file.readlines()
+
+    for code in codes:
+        print(code)
+        direction = code[0]
+        distance = int(code[1:])
+        print(direction, distance)
+        if direction == "L":
+            for i in range(distance):
+                if position == 0:
+                    position = 99
+                else:
+                    position -= 1
+                    if position == 0:
+                        zeros += 1
+        elif direction == "R":
+            for i in range(distance):
+                if position == 99:
+                    position = 0
+                    zeros += 1
+                else:
+                    position += 1
+        
+        
+    print(zeros)
 
 
 if __name__ == "__main__":
